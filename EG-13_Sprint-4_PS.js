7c4c3ae0590b2805f731699506ada9ae3cfab376
SportNest <student@sportnest.com>
2026-09-11 13:13:37 +0600

    problem no 1 is solve

---
+++ EG-13_Sprint-4_PS.js
@@ -0,0 +1,30 @@
+// 01. Isomorphic Strings
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    if (s.length !== t.length) return false;
+    
+    const mapST = new Map();
+    const mapTS = new Map();
+    
+    for (let i = 0; i < s.length; i++) {
+        const charS = s[i];
+        const charT = t[i];
+        
+        if ((mapST.has(charS) && mapST.get(charS) !== charT) ||
+            (mapTS.has(charT) && mapTS.get(charT) !== charS)) {
+            return false;
+        }
+        
+        mapST.set(charS, charT);
+        mapTS.set(charT, charS);
+    }
+    
+    return true;
+};
+
+// console.log("('paper', 'title'):", isIsomorphic("paper", "title"));
