58bd5e08182df7d368318155c6d77fbe716f8c01
SportNest <student@sportnest.com>
2026-09-13 00:55:09 +0600

    all problem are solve

--- EG-13_Sprint-4_PS.js
+++ EG-13_Sprint-4_PS.js
@@ -38,32 +38,32 @@ function listToArray(head) {
 
 // 01. Isomorphic Strings
 
-// /**
-//  * @param {string} s
-//  * @param {string} t
-//  * @return {boolean}
-//  */
-// var isIsomorphic = function(s, t) {
-//     if (s.length !== t.length) return false;
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    if (s.length !== t.length) return false;
     
-//     const mapST = new Map();
-//     const mapTS = new Map();
+    const mapST = new Map();
+    const mapTS = new Map();
     
-//     for (let i = 0; i < s.length; i++) {
-//         const charS = s[i];
-//         const charT = t[i];
+    for (let i = 0; i < s.length; i++) {
+        const charS = s[i];
+        const charT = t[i];
         
-//         if ((mapST.has(charS) && mapST.get(charS) !== charT) ||
-//             (mapTS.has(charT) && mapTS.get(charT) !== charS)) {
-//             return false;
-//         }
+        if ((mapST.has(charS) && mapST.get(charS) !== charT) ||
+            (mapTS.has(charT) && mapTS.get(charT) !== charS)) {
+            return false;
+        }
         
-//         mapST.set(charS, charT);
-//         mapTS.set(charT, charS);
-//     }
+        mapST.set(charS, charT);
+        mapTS.set(charT, charS);
+    }
     
-//     return true;
-// };
+    return true;
+};
 
 // console.log("('paper', 'title'):", isIsomorphic("paper", "title"));
 
@@ -73,33 +73,33 @@ function listToArray(head) {
 
 // 02. Word Pattern
 
-// /**
-//  * @param {string} pattern
-//  * @param {string} s
-//  * @return {boolean}
-//  */
-// var wordPattern = function(pattern, s) {
-//     const words = s.split(' ');
-//     if (pattern.length !== words.length) return false;
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, s) {
+    const words = s.split(' ');
+    if (pattern.length !== words.length) return false;
     
-//     const charToWord = new Map();
-//     const wordToChar = new Map();
+    const charToWord = new Map();
+    const wordToChar = new Map();
     
-//     for (let i = 0; i < pattern.length; i++) {
-//         const char = pattern[i];
-//         const word = words[i];
+    for (let i = 0; i < pattern.length; i++) {
+        const char = pattern[i];
+        const word = words[i];
         
-//         if ((charToWord.has(char) && charToWord.get(char) !== word) ||
-//             (wordToChar.has(word) && wordToChar.get(word) !== char)) {
-//             return false;
-//         }
+        if ((charToWord.has(char) && charToWord.get(char) !== word) ||
+            (wordToChar.has(word) && wordToChar.get(word) !== char)) {
+            return false;
+        }
         
-//         charToWord.set(char, word);
-//         wordToChar.set(word, char);
-//     }
+        charToWord.set(char, word);
+        wordToChar.set(word, char);
+    }
     
-//     return true;
-// };
+    return true;
+};
 
 // console.log("('abba', 'dog cat cat dog'):", wordPattern("abba", "dog cat cat dog"));
 
@@ -108,23 +108,23 @@ function listToArray(head) {
 
 // 03. Find the Difference
 
-// /**
-//  * @param {string} s
-//  * @param {string} t
-//  * @return {character}
-//  */
-// var findTheDifference = function(s, t) {
-//     let xorVal = 0;
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function(s, t) {
+    let xorVal = 0;
     
-//     for (let i = 0; i < s.length; i++) {
-//         xorVal ^= s.charCodeAt(i);
-//     }
-//     for (let i = 0; i < t.length; i++) {
-//         xorVal ^= t.charCodeAt(i);
-//     }
+    for (let i = 0; i < s.length; i++) {
+        xorVal ^= s.charCodeAt(i);
+    }
+    for (let i = 0; i < t.length; i++) {
+        xorVal ^= t.charCodeAt(i);
+    }
     
-//     return String.fromCharCode(xorVal);
-// };
+    return String.fromCharCode(xorVal);
+};
 
 // console.log("('abcd', 'abcde'):", findTheDifference("abcd", "abcde"));
 
@@ -134,23 +134,23 @@ function listToArray(head) {
 
 // 04. Reverse Linked List
 
-// /**
-//  * @param {ListNode} head
-//  * @return {ListNode}
-//  */
-// var reverseList = function(head) {
-//     let prev = null;
-//     let curr = head;
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let prev = null;
+    let curr = head;
     
-//     while (curr !== null) {
-//         let nextTemp = curr.next;
-//         curr.next = prev;
-//         prev = curr;
-//         curr = nextTemp;
-//     }
+    while (curr !== null) {
+        let nextTemp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = nextTemp;
+    }
     
-//     return prev;
-// };
+    return prev;
+};
 
 // console.log("([1, 2, 3, 6, 8, 3, 9]):", listToArray(reverseList(arrayToList([1, 2, 3, 6, 8, 3, 9]))));
 
@@ -160,21 +160,21 @@ function listToArray(head) {
 
 // 05. Middle of the Linked List
 
-// /**
-//  * @param {ListNode} head
-//  * @return {ListNode}
-//  */
-// var middleNode = function(head) {
-//     let slow = head;
-//     let fast = head;
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let slow = head;
+    let fast = head;
     
-//     while (fast !== null && fast.next !== null) {
-//         slow = slow.next;
-//         fast = fast.next.next;
-//     }
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
     
-//     return slow;
-// };
+    return slow;
+};
  
 // console.log(" ([1, 2, 3, 4, 5, 6]):", listToArray(middleNode(arrayToList([1, 2, 3, 4, 5, 6]))));
 
@@ -186,28 +186,28 @@ function listToArray(head) {
 // 06. Product of Array Except Self
 
 
-// /**
-//  * @param {number[]} nums
-//  * @return {number[]}
-//  */
-// var productExceptSelf = function(nums) {
-//     const n = nums.length;
-//     const res = new Array(n).fill(1);
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    const n = nums.length;
+    const res = new Array(n).fill(1);
     
-//     let prefix = 1;
-//     for (let i = 0; i < n; i++) {
-//         res[i] = prefix;
-//         prefix *= nums[i];
-//     }
+    let prefix = 1;
+    for (let i = 0; i < n; i++) {
+        res[i] = prefix;
+        prefix *= nums[i];
+    }
     
-//     let postfix = 1;
-//     for (let i = n - 1; i >= 0; i--) {
-//         res[i] *= postfix;
-//         postfix *= nums[i];
-//     }
+    let postfix = 1;
+    for (let i = n - 1; i >= 0; i--) {
+        res[i] *= postfix;
+        postfix *= nums[i];
+    }
     
-//     return res;
-// };
+    return res;
+};
 
 
 // console.log("([-1, 1, 0, -3, 3]):", productExceptSelf([-1, 1, 0, -3, 3]));
@@ -221,29 +221,29 @@ function listToArray(head) {
 // 07. Remove Nth Node From End of List
 
 
-// /**
-//  * @param {ListNode} head
-//  * @param {number} n
-//  * @return {ListNode}
-//  */
-// var removeNthFromEnd = function(head, n) {
-//     const dummy = new ListNode(0, head);
-//     let fast = dummy;
-//     let slow = dummy;
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    const dummy = new ListNode(0, head);
+    let fast = dummy;
+    let slow = dummy;
     
-//     for (let i = 0; i <= n; i++) {
-//         fast = fast.next;
-//     }
+    for (let i = 0; i <= n; i++) {
+        fast = fast.next;
+    }
     
-//     while (fast !== null) {
-//         slow = slow.next;
-//         fast = fast.next;
-//     }
+    while (fast !== null) {
+        slow = slow.next;
+        fast = fast.next;
+    }
     
-//     slow.next = slow.next.next;
+    slow.next = slow.next.next;
     
-//     return dummy.next;
-// };
+    return dummy.next;
+};
 
 
 // console.log("listArray", listToArray(removeNthFromEnd(arrayToList([1, 2, 3, 4, 5]), 2)));
@@ -255,36 +255,36 @@ function listToArray(head) {
 
 // 08. Find First and Last Position of Element in Sorted Array
 
-// /**
-//  * @param {number[]} nums
-//  * @param {number} target
-//  * @return {number[]}
-//  */
-// var searchRange = function(nums, target) {
-//     const findBound = (isFirst) => {
-//         let left = 0, right = nums.length - 1;
-//         let bound = -1;
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    const findBound = (isFirst) => {
+        let left = 0, right = nums.length - 1;
+        let bound = -1;
         
-//         while (left <= right) {
-//             let mid = Math.floor((left + right) / 2);
-//             if (nums[mid] === target) {
-//                 bound = mid;
-//                 if (isFirst) {
-//                     right = mid - 1;
-//                 } else {
-//                     left = mid + 1;
-//                 }
-//             } else if (nums[mid] < target) {
-//                 left = mid + 1;
-//             } else {
-//                 right = mid - 1;
-//             }
-//         }
-//         return bound;
-//     };
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2);
+            if (nums[mid] === target) {
+                bound = mid;
+                if (isFirst) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return bound;
+    };
     
-//     return [findBound(true), findBound(false)];
-// };
+    return [findBound(true), findBound(false)];
+};
 
 // console.log("([5,7,7,8,8,10]", searchRange([5, 7, 7, 8, 8, 10], 8));
 
@@ -293,50 +293,50 @@ function listToArray(head) {
 
 // 09. Permutation in String 
 
-// /**
-//  * @param {string} s1
-//  * @param {string} s2
-//  * @return {boolean}
-//  */
-// var checkInclusion = function(s1, s2) {
-//     if (s1.length > s2.length) return false;
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = function(s1, s2) {
+    if (s1.length > s2.length) return false;
     
-//     const count1 = new Array(26).fill(0);
-//     const count2 = new Array(26).fill(0);
+    const count1 = new Array(26).fill(0);
+    const count2 = new Array(26).fill(0);
     
-//     for (let i = 0; i < s1.length; i++) {
-//         count1[s1.charCodeAt(i) - 97]++;
-//         count2[s2.charCodeAt(i) - 97]++;
-//     }
+    for (let i = 0; i < s1.length; i++) {
+        count1[s1.charCodeAt(i) - 97]++;
+        count2[s2.charCodeAt(i) - 97]++;
+    }
     
-//     let matches = 0;
-//     for (let i = 0; i < 26; i++) {
-//         if (count1[i] === count2[i]) matches++;
-//     }
+    let matches = 0;
+    for (let i = 0; i < 26; i++) {
+        if (count1[i] === count2[i]) matches++;
+    }
     
-//     for (let i = 0; i < s2.length - s1.length; i++) {
-//         if (matches === 26) return true;
+    for (let i = 0; i < s2.length - s1.length; i++) {
+        if (matches === 26) return true;
         
-//         const leftIdx = s2.charCodeAt(i) - 97;
-//         const rightIdx = s2.charCodeAt(i + s1.length) - 97;
+        const leftIdx = s2.charCodeAt(i) - 97;
+        const rightIdx = s2.charCodeAt(i + s1.length) - 97;
         
-//         count2[rightIdx]++;
-//         if (count2[rightIdx] === count1[rightIdx]) {
-//             matches++;
-//         } else if (count2[rightIdx] === count1[rightIdx] + 1) {
-//             matches--;
-//         }
+        count2[rightIdx]++;
+        if (count2[rightIdx] === count1[rightIdx]) {
+            matches++;
+        } else if (count2[rightIdx] === count1[rightIdx] + 1) {
+            matches--;
+        }
         
-//         count2[leftIdx]--;
-//         if (count2[leftIdx] === count1[leftIdx]) {
-//             matches++;
-//         } else if (count2[leftIdx] === count1[leftIdx] - 1) {
-//             matches--;
-//         }
-//     }
+        count2[leftIdx]--;
+        if (count2[leftIdx] === count1[leftIdx]) {
+            matches++;
+        } else if (count2[leftIdx] === count1[leftIdx] - 1) {
+            matches--;
+        }
+    }
     
-//     return matches === 26;
-// };
+    return matches === 26;
+};
 
 
 
