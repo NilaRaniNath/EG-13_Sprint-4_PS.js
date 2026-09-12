537040615b95399340c5b8dafe1e4ab7f326fe65
SportNest <student@sportnest.com>
2026-09-12 18:57:06 +0600

    number of problem no 2 solve

--- EG-13_Sprint-4_PS.js
+++ EG-13_Sprint-4_PS.js
@@ -1,30 +1,66 @@
 // 01. Isomorphic Strings
 
+// /**
+//  * @param {string} s
+//  * @param {string} t
+//  * @return {boolean}
+//  */
+// var isIsomorphic = function(s, t) {
+//     if (s.length !== t.length) return false;
+    
+//     const mapST = new Map();
+//     const mapTS = new Map();
+    
+//     for (let i = 0; i < s.length; i++) {
+//         const charS = s[i];
+//         const charT = t[i];
+        
+//         if ((mapST.has(charS) && mapST.get(charS) !== charT) ||
+//             (mapTS.has(charT) && mapTS.get(charT) !== charS)) {
+//             return false;
+//         }
+        
+//         mapST.set(charS, charT);
+//         mapTS.set(charT, charS);
+//     }
+    
+//     return true;
+// };
+
+// console.log("('paper', 'title'):", isIsomorphic("paper", "title"));
+
+
+
+
+
+// 02. Word Pattern
+
 /**
+ * @param {string} pattern
  * @param {string} s
- * @param {string} t
  * @return {boolean}
  */
-var isIsomorphic = function(s, t) {
-    if (s.length !== t.length) return false;
+var wordPattern = function(pattern, s) {
+    const words = s.split(' ');
+    if (pattern.length !== words.length) return false;
     
-    const mapST = new Map();
-    const mapTS = new Map();
+    const charToWord = new Map();
+    const wordToChar = new Map();
     
-    for (let i = 0; i < s.length; i++) {
-        const charS = s[i];
-        const charT = t[i];
+    for (let i = 0; i < pattern.length; i++) {
+        const char = pattern[i];
+        const word = words[i];
         
-        if ((mapST.has(charS) && mapST.get(charS) !== charT) ||
-            (mapTS.has(charT) && mapTS.get(charT) !== charS)) {
+        if ((charToWord.has(char) && charToWord.get(char) !== word) ||
+            (wordToChar.has(word) && wordToChar.get(word) !== char)) {
             return false;
         }
         
-        mapST.set(charS, charT);
-        mapTS.set(charT, charS);
+        charToWord.set(char, word);
+        wordToChar.set(word, char);
     }
     
     return true;
 };
 
-// console.log("('paper', 'title'):", isIsomorphic("paper", "title"));
+console.log("('abba', 'dog cat cat dog'):", wordPattern("abba", "dog cat cat dog"));
