8a0a5daf33dfce403ada2fc7dec046e10e9e7242
SportNest <student@sportnest.com>
2026-09-12 19:25:28 +0600

    number of problem no 3 solve

--- EG-13_Sprint-4_PS.js
+++ EG-13_Sprint-4_PS.js
@@ -35,32 +35,57 @@
 
 // 02. Word Pattern
 
+// /**
+//  * @param {string} pattern
+//  * @param {string} s
+//  * @return {boolean}
+//  */
+// var wordPattern = function(pattern, s) {
+//     const words = s.split(' ');
+//     if (pattern.length !== words.length) return false;
+    
+//     const charToWord = new Map();
+//     const wordToChar = new Map();
+    
+//     for (let i = 0; i < pattern.length; i++) {
+//         const char = pattern[i];
+//         const word = words[i];
+        
+//         if ((charToWord.has(char) && charToWord.get(char) !== word) ||
+//             (wordToChar.has(word) && wordToChar.get(word) !== char)) {
+//             return false;
+//         }
+        
+//         charToWord.set(char, word);
+//         wordToChar.set(word, char);
+//     }
+    
+//     return true;
+// };
+
+// console.log("('abba', 'dog cat cat dog'):", wordPattern("abba", "dog cat cat dog"));
+
+
+
+
+// 03. Find the Difference
+
 /**
- * @param {string} pattern
  * @param {string} s
- * @return {boolean}
+ * @param {string} t
+ * @return {character}
  */
-var wordPattern = function(pattern, s) {
-    const words = s.split(' ');
-    if (pattern.length !== words.length) return false;
+var findTheDifference = function(s, t) {
+    let xorVal = 0;
     
-    const charToWord = new Map();
-    const wordToChar = new Map();
-    
-    for (let i = 0; i < pattern.length; i++) {
-        const char = pattern[i];
-        const word = words[i];
-        
-        if ((charToWord.has(char) && charToWord.get(char) !== word) ||
-            (wordToChar.has(word) && wordToChar.get(word) !== char)) {
-            return false;
-        }
-        
-        charToWord.set(char, word);
-        wordToChar.set(word, char);
+    for (let i = 0; i < s.length; i++) {
+        xorVal ^= s.charCodeAt(i);
+    }
+    for (let i = 0; i < t.length; i++) {
+        xorVal ^= t.charCodeAt(i);
     }
     
-    return true;
+    return String.fromCharCode(xorVal);
 };
 
-console.log("('abba', 'dog cat cat dog'):", wordPattern("abba", "dog cat cat dog"));
+// console.log("('abcd', 'abcde'):", findTheDifference("abcd", "abcde"));
